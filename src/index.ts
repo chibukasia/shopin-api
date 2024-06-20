c07706dd841612634a36761d4ba8ec45751d4b36
nelson chibukasia <nelson.chibukasia@globallinkplus.com>
2024-06-20 22:09:11 +0300

    added prisma orm

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const port = process.env.PORT || 8000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.json("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
