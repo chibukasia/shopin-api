90f7619d75e9de03a53122ba1bc91fdaa3a3a828
nelson chibukasia <nelson.chibukasia@globallinkplus.com>
2024-09-03 02:02:47 +0300

    added errror handling

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -4,6 +4,7 @@ import bycrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { z, ZodIssue } from "zod";
 import { userSchema } from "../validators/user-validator";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 const prismaClient = new PrismaClient().$extends({
   query: {
@@ -36,8 +37,13 @@ const createToken = (id: number) => {
 };
 
 export const getAllUsers = async (req: Request, res: Response) => {
-  const users = await prismaClient.user.findMany();
-  res.json(users);
+  try {
+    const users = await prismaClient.user.findMany();
+  res.status(200).json(users);
+  } catch (error) {
+    userErrorHandler(error, res)
+  }
+  
 };
 
 export const createUser = async (req: Request, res: Response) => {
@@ -55,61 +61,101 @@ export const createUser = async (req: Request, res: Response) => {
       data: user,
     });
     const token = createToken(newUser.id);
-    res.json({ ...newUser, token });
+    res.status(201).json({ ...newUser, token });
   } catch (error: any) {
-    res.json(error?.issues.map((issue: ZodIssue) => issue.message));
+    // res.json(error?.issues.map((issue: ZodIssue) => issue.message));
+    userErrorHandler(error, res)
   }
 };
 
 export const loginUser = async (req: Request, res: Response) => {
-  const loggedInUser = await prismaClient.user.findUnique({
-    where: {
-      email: req.body.email,
-    },
-  });
-
-  if (loggedInUser) {
-    const salt = await bycrypt.genSalt();
-    const password = await bycrypt.compare(
-      req.body.password,
-      loggedInUser.password
-    );
-    if (password) {
-      const token = createToken(loggedInUser.id);
-      res.json({ ...loggedInUser, token });
+  try {
+    const loggedInUser = await prismaClient.user.findUnique({
+      where: {
+        email: req.body.email,
+      },
+    });
+  
+    if (loggedInUser) {
+      const salt = await bycrypt.genSalt();
+      const password = await bycrypt.compare(
+        req.body.password,
+        loggedInUser.password
+      );
+      if (password) {
+        const token = createToken(loggedInUser.id);
+        res.status(201).json({ ...loggedInUser, token });
+      } else {
+        res.status(401).json({ error: "Invalid password" });
+      }
     } else {
-      res.status(401).json({ error: "Invalid password" });
+      res.status(404).json({ error: "Email does not exist" });
     }
-  } else {
-    res.status(404).json({ error: "Email does not exist" });
+  } catch (error) {
+    userErrorHandler(error, res)
   }
+  
 };
 
 export const getUser = async (req: Request, res: Response) => {
-  const user = await prismaClient.user.findFirst({
-    where: {
-      id: Number(req.params.id),
-    },
-  });
-  res.json(user);
+
+  try {
+    const user = await prismaClient.user.findFirst({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json(user);
+    
+  } catch (error) {
+    userErrorHandler(error, res)
+  }
+  
 };
 
 export const updateUser = async (req: Request, res: Response) => {
-  const user = await prismaClient.user.update({
-    where: {
-      id: Number(req.params.id),
-    },
-    data: req.body,
-  });
-
-  res.json(user);
+  try {
+    const user = await prismaClient.user.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: req.body,
+    });
+  
+    res.status(201).json(user);
+  } catch (error) {
+    userErrorHandler(error, res)
+  }
+  
 };
 
 export const deleteUser = async (req: Request, res: Response) => {
-  await prismaClient.user.delete({
-    where: {
-      id: Number(req.params.id),
-    },
-  });
-  res.status(201).json({ message: "User deleted successfully" });
+  try {
+    await prismaClient.user.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(204).json({ message: "User deleted successfully" });
+  } catch (error) {
+    userErrorHandler(error, res)
+  }
+  
 };
+
+
+const userErrorHandler = (error: any, res: Response) => {
+  if (error instanceof PrismaClientKnownRequestError){
+    if (error.code === 'P2002'){
+      res.json({error: 'Email already taken'})
+    }
+    if (error.code === 'P2001'){
+      res.json({error: 'User not found'})
+    }
+    if(error.code === 'P2011'){
+      res.json({error: `${error.message}`})
+    }
+  }else{
+    res.json({error: 'Something went wrong'})
+  }
+}
